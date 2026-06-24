56fbad063a2d58193ad2d52489d008ad27a853ce
Pirsona <LerouSanchez@yandex.ru>
2026-06-24 20:31:02 +0500

    Refactor TextChanger

--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -7,21 +7,26 @@ using UnityEngine.UI;
 
 public class TextChanger : MonoBehaviour
 {
+    
     [SerializeField] private Text _text;
-    [SerializeField] private string[] _newText;
+    [SerializeField] private string _newText;
+    [SerializeField] private string _addText;
+    [SerializeField] private string _removeText;
     [SerializeField] private float _duration;
-    [SerializeField] private ScrambleMode[] _scrambleMode;
+    [SerializeField] private ScrambleMode _scrambleMode;
+    [SerializeField] private bool _richTextEnabled;
+    [SerializeField] private int _loop;
+    [SerializeField] private LoopType _loopType;
+    
     private void Start()
-    {
-        while (true)
-        {
-            int currentIndex = 0;
-            
-            foreach (string text in _newText )
-            {
-                _text.DOText(text,_duration,false,_scrambleMode[currentIndex]);
-                currentIndex++;
-            }
-        }
+    { 
+        Sequence textSequence = DOTween.Sequence();
+        
+        textSequence.Append(_text.DOText(_newText, _duration));
+        textSequence.AppendInterval(_duration);
+        textSequence.Append(_text.DOText(" "  + _addText, _duration).SetRelative());
+        textSequence.AppendInterval(_duration);
+        textSequence.Append(_text.DOText(_removeText, _duration, _richTextEnabled, _scrambleMode));
+        textSequence.SetLoops(_loop, _loopType);
     }
 }
